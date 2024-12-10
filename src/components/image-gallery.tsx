d6c56130b8c412f2c439e78df73c8a7a2e7d17b6
ネコにいさんチャンネル <nekoniisanb@gmail.com>
2024-12-10 14:18:18 +0900

    Update image-gallery.tsx

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -370,6 +370,7 @@ export default function Component() {
             </div>
           ))}
         </div>
+        <div><hr/></div>
         <div className="mt-8 flex justify-center items-center space-x-2">
           <Button
             onClick={() => handlePageChange(currentPage - 1)}
