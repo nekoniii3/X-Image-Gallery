1be1813ed6942201d9a1d818c5f4a9259ea38698
ネコにいさんチャンネル <nekoniisanb@gmail.com>
2024-12-10 14:38:29 +0900

    Update image-gallery.tsx

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -398,6 +398,7 @@ export default function Component() {
           <Button type="submit" className="">
               Search
           </Button>
+          <button className="bg-black">Next &gt;&gt;&gt;</button>
         </div>
         <ImageModal post={selectedPost} onClose={handleCloseModal} />
       </div>
